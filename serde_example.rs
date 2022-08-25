bb278ef8f9840ea1327f0a40235e691f08d8ec9e
Polo <59456711+PaulB86UK@users.noreply.github.com>
2022-08-26 00:40:51 +0100

    Update serde_example.rs
    *check if the instances match
    *Added formatting to be able to use the display with a better formatted representation of the struct used

--- serde_example.rs
+++ serde_example.rs
@@ -16,12 +16,29 @@ fn main() {
         add_up_to: 100,
         done: false,
     };
-
+    //implement display trait for the struct work so it can be easily used
+    //whe using the println macro
+    impl std::fmt::Display for Work {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(
+                f,
+                "\n---\nWork:\n id: {}\n work_code: {}\n add_up_to: {}\n done: {}\n---",
+                self.id, self.work_code, self.add_up_to, self.done,
+            )
+        }
+    }
     //serialize the struct into a JSON String
     let json_str = serde_json::to_string(&work).unwrap();
     println!("Serialize: {}", json_str);
 
     //deserialize the struct into a JSON String
     let work_from_json_str: Work = serde_json::from_str(json_str.as_str()).unwrap();
-    println!("DeSerialize: {:?}", work_from_json_str);
+    println!("DeSerialize: {:?}", work_from_json_str); //{:?} TODO Implement Display
+
+    //check if the instances match
+    assert_eq!(work, work_from_json_str);
+
+    //print
+    let work_from_json_str2: Work = serde_json::from_str(json_str.as_str()).unwrap();
+    println!("DeSerialize v2: {}", work_from_json_str2); //Using Display this time
 }
